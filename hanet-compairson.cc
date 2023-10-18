aac7b7cfd6b88e7095fde85e7d5949d9b4b704b0
jyarac <jyarac@unal.edu.co>
2023-10-17 19:54:28 -0500

    Creacion de nodos moviles en cada nodo adhoc

--- hanet-compairson.cc
+++ hanet-compairson.cc
@@ -32,6 +32,7 @@ main(int argc, char* argv[])
 {
     int nodeSpeed = 20; // in m/s
     int nodePause = 0; 
+    int movilNodes = 2;
     //declaring routing protocols
     AodvHelper aodv;
     OlsrHelper olsr;
@@ -133,9 +134,76 @@ main(int argc, char* argv[])
     {
         NS_FATAL_ERROR("No such protocol:" << m_protocolName);
     }
+    Ipv4AddressHelper ipAddrs;
+    ipAddrs.SetBase("192.168.0.0", "255.255.255.0");
+    ipAddrs.Assign(manetDevices);
+    ipAddrs.SetBase("172.16.0.0", "255.255.255.0");
+    for (uint32_t i = 0; i < manetNodes; ++i)
+    {
+        NS_LOG_INFO("Configuring wireless network for manet node " << i);
+        //
+        // Create a container to manage the nodes of the LAN.  We need
+        // two containers here; one with all of the new nodes, and one
+        // with all of the nodes including new and existing nodes
+        //
+        NodeContainer stas;
+        stas.Create(movilNodes - 1);
+        // Now, create the container with all nodes on this link
+        NodeContainer movil(adhocContainer.Get(i), stas);
+        //
+        // Create an infrastructure network
+        //
+        WifiHelper wifimovil;
+        WifiMacHelper macmovil;
+        wifiPhy.SetChannel(wifiChannel.Create());
+        // Create unique ssids for these networks
+        std::string ssidString("wifi-movil");
+        std::stringstream ss;
+        ss << i;
+        ssidString += ss.str();
+        Ssid ssid = Ssid(ssidString);
+        // setup stas
+        macmovil.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid));
+        NetDeviceContainer staDevices = wifimovil.Install(wifiPhy, macmovil, stas);
+        // setup ap.
+        macmovil.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
+        NetDeviceContainer apDevices = wifimovil.Install(wifiPhy, macmovil, adhocContainer.Get(i));
+        // Collect all of these new devices
+        NetDeviceContainer movilDevices(apDevices, staDevices);
 
-//clusters lefts
-
-
+        // Add the IPv4 protocol stack to the nodes in our container
+        //
+        internet.Install(stas);
+        //
+        // Assign IPv4 addresses to the device drivers (actually to the associated
+        // IPv4 interfaces) we just created.
+        //
+        ipAddrs.Assign(movilDevices);
+        //
+        // Assign a new network prefix for each movil network, according to
+        // the network mask initialized above
+        //
+        ipAddrs.NewNetwork();
+        //
+        // The new wireless nodes need a mobility model so we aggregate one
+        // to each of the nodes we just finished building.
+        //
+        Ptr<ListPositionAllocator> subnetAlloc = CreateObject<ListPositionAllocator>();
+        for (uint32_t j = 0; j < movil.GetN(); ++j)
+        {
+            subnetAlloc->Add(Vector(0.0, j, 0.0));
+        }
+        
+        mobilityAdhoc.PushReferenceMobilityModel(adhocContainer.Get(i));
+        mobilityAdhoc.SetPositionAllocator(subnetAlloc);
+        mobilityAdhoc.SetMobilityModel("ns3::RandomDirection2dMobilityModel",
+                                  "Bounds",
+                                  RectangleValue(Rectangle(-10, 10, -10, 10)),
+                                  "Speed",
+                                  StringValue("ns3::ConstantRandomVariable[Constant=3]"),
+                                  "Pause",
+                                  StringValue("ns3::ConstantRandomVariable[Constant=0.4]"));
+        mobilityAdhoc.Install(stas);
+    }
 
 }
